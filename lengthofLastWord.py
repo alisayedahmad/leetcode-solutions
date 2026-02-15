2e19dd445e3487a87ee8d12d0a90dc1e05a14ac3
ASA <alisaydahmad10@gmail.com>
2026-02-16 00:44:04 +0100

    Length of Last Word

---
+++ lengthofLastWord.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s = s.strip()
+        if not s:
+            return 0
+        return len(s.split()[-1])
+
+# Test
+if __name__ == "__main__":
+    solution = Solution()
+    result = solution.lengthOfLastWord("Hello World")
+    print(result) 
